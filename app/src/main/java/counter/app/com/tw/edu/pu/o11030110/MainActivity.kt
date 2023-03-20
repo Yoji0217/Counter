74111b78bfa0f1c95927af3cc6bfe21302fc37ea
kiuchiyoji <o1103011@gm.pu.edu.tw>
2023-03-20 11:20:02 +0800

    加入 樂透

--- app/src/main/java/counter/app/com/tw/edu/pu/o11030110/MainActivity.kt
+++ app/src/main/java/counter/app/com/tw/edu/pu/o11030110/MainActivity.kt
@@ -2,13 +2,19 @@ package counter.app.com.tw.edu.pu.o11030110
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var txv:TextView=findViewById(R.id.txv)
-        txv.text="0"
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = "0"
     }
-}
+        fun happy(v: View){
+            var txv: TextView = findViewById(R.id.txv)
+            txv.text = (1..100).random().toString()
+        }
+    }
+
